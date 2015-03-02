455999ea7b528cedbb5e266e640683f7084182b6
Mauricio Coria <coriamauricio@gmail.com>
2015-03-02 02:49:30 -0500

    Agrego restservlet-client
    Agrego restservlet-client

---
+++ java/restservlet-client/restservlet-client/src/main/java/asd/restservlet_client/App.java
@@ -0,0 +1,13 @@
+package asd.restservlet_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
