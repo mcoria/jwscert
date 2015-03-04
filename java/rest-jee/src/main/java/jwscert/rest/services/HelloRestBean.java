e3250a1d82dd04e8a0c864135024437df0927f52
Coria Alvarez, Mauricio <Mauricioca@IADB.ORG>
2015-03-04 18:22:40 -0500

    ASD

---
+++ java/rest-jee/src/main/java/jwscert/rest/services/HelloRestBean.java
@@ -0,0 +1,19 @@
+package jwscert.rest.services;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("hello")
+public class HelloRestBean {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWithParams(){
+		return "Hello World";
+	}
+	
+}
