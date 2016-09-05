1fbe71f2138b87fbb725e413f6cebda8bca989b6
Mauricio Coria <coriamauricio@gmail.com>
2016-09-05 17:28:30 -0400

    MTOM tests

--- java/soap/soap-mtom/src/main/java/jwscert/jaxws/mtom/services/ServiceImp1.java
+++ java/soap/soap-mtom/src/main/java/jwscert/jaxws/mtom/services/ServiceImp1.java
@@ -5,18 +5,15 @@ import java.io.InputStream;
 
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
-import javax.xml.ws.soap.MTOM;
-import javax.xml.ws.soap.SOAPBinding;
 
 import com.google.common.io.ByteStreams;
 
 import jwscert.jaxws.mtom.types.Image;
 
 
-@WebService(name = "Service")
-@MTOM(enabled=true, threshold=1024)
-@BindingType(value=SOAPBinding.SOAP11HTTP_MTOM_BINDING)
-public class ServiceImp {
+@WebService(serviceName = "Service1")
+@BindingType(value=javax.xml.ws.soap.SOAPBinding.SOAP11HTTP_MTOM_BINDING)
+public class ServiceImp1 {
 	public Image getImage() {
 		Image result = new Image();
 		result.setImageName("PNG3486.png");
